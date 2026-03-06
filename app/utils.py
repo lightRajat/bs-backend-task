405e050eadf5142cab4d52fddeaaede345af029a
Rajat <light.rajat+gh@gmail.com>
2026-03-06 11:48:42 +0530

    feat: Response now returns primary contacts as the first element

--- app/utils.py
+++ app/utils.py
@@ -8,26 +8,38 @@ def log(msg: str, is_error: bool = False) -> None:
         print(f"🟢  [LOG] {msg}")
 
 def create_contact_response_schema(contacts: list[Contact]) -> ContactSchema:
-    primary_contact_id: int
+    for i in range(len(contacts)):
+        if contacts[i].linkPrecedence == "primary":
+            primary_contact = contacts[i]
+            break
+    
+    primary_contact_id = primary_contact.id
     emails = set()
     phone_numbers = set()
     secondary_contact_ids = []
 
-    for contact in contacts:
-        if contact.linkPrecedence == "primary":
-            primary_contact_id = contact.id
-        else:
-            secondary_contact_ids.append(contact.id)
-        
-        if contact.email:
-            emails.add(contact.email)
-        if contact.phoneNumber:
-            phone_numbers.add(contact.phoneNumber)
+    for c in contacts:
+        if c.email:
+            emails.add(c.email)
+        if c.phoneNumber:
+            phone_numbers.add(c.phoneNumber)
+        if c.linkPrecedence != 'primary':
+            secondary_contact_ids.append(c.id)
+    
+    emails = list(emails)
+    if primary_contact.email:
+        emails.remove(primary_contact.email)
+        emails.insert(0, primary_contact.email)
+
+    phone_numbers = list(phone_numbers)
+    if primary_contact.phoneNumber:
+        phone_numbers.remove(primary_contact.phoneNumber)
+        phone_numbers.insert(0, primary_contact.phoneNumber)
 
     schema = ContactSchema(
         primaryContactId=primary_contact_id,
-        emails=list(emails),
-        phoneNumbers=list(phone_numbers),
+        emails=emails,
+        phoneNumbers=phone_numbers,
         secondaryContactIds=secondary_contact_ids
     )
 
